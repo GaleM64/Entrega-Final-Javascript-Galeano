47d3954d4120163dd0bcfbc0b5936801c5d53cca
Martin Galeano <galeano.m2000@gmail.com>
2024-11-05 04:56:44 -0300

    fiximage

--- js/constantes/valoresmejoras.js
+++ js/constantes/valoresmejoras.js
@@ -1,7 +1,7 @@
 export const valoresmejoras = [
-    {name:'clicker', image: '../assets/clicker-white.png', cost: 10, increase: 1},
-    {name:'pamasar', image: '../assets/pamasar.png', cost: 60, increase: 4},
-    {name:'panadero', image: '../assets/panadero.png', cost: 380, increase: 16},
-    {name:'panaderia', image: '../assets/panaderia.png', cost: 3105, increase: 82},
-    {name:'festival', image: '../assets/festival.png', cost: 23105, increase: 422},
+    {name:'clicker', image: '/assets/clicker-white.png', cost: 10, increase: 1},
+    {name:'pamasar', image: '/assets/pamasar.png', cost: 60, increase: 4},
+    {name:'panadero', image: '/assets/panadero.png', cost: 380, increase: 16},
+    {name:'panaderia', image: '/assets/panaderia.png', cost: 3105, increase: 82},
+    {name:'festival', image: '/assets/festival.png', cost: 23105, increase: 422},
 ]
