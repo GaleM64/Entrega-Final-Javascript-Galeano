f5bbe66dd7c86f13f608baf082f2473fb3e27ade
Martin Galeano <galeano.m2000@gmail.com>
2024-11-05 04:44:50 -0300

    funcionplantilla

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { upgrades } from "./constantes/upgrades.js"
+
 let torta = document.querySelector(".torta-cost")
 let parsedTorta = parseFloat(torta.innerHTML)
 
@@ -9,50 +11,10 @@ let tpc = 1;
 
 let tps = 0;
 
-const upgrades = [
-    {
-        name: "clicker",
-        cost: document.querySelector(".clicker-cost"),
-        parsedCost: parseFloat(document.querySelector(".clicker-cost").innerHTML),
-        increase: document.querySelector(".clicker-increase"),
-        parsedIncrease: parseFloat(document.querySelector(".clicker-increase").innerHTML),
-        level: document.querySelector(".clicker-level"),
-        tortaMultiplier: 1.025,
-        costMultiplier: 1.12,
-    },
-    {
-        name: "pamasar",
-        cost: document.querySelector(".pamasar-cost"),
-        parsedCost: parseFloat(document.querySelector(".pamasar-cost").innerHTML),
-        increase: document.querySelector(".pamasar-increase"),
-        parsedIncrease: parseFloat(document.querySelector(".pamasar-increase").innerHTML),
-        level: document.querySelector(".pamasar-level"),
-        tortaMultiplier: 1.03,
-        costMultiplier: 1.115,
-    },
-    {
-        name: "panadero",
-        cost: document.querySelector(".panadero-cost"),
-        parsedCost: parseFloat(document.querySelector(".panadero-cost").innerHTML),
-        increase: document.querySelector(".panadero-increase"),
-        parsedIncrease: parseFloat(document.querySelector(".panadero-increase").innerHTML),
-        level: document.querySelector(".panadero-level"),
-        tortaMultiplier: 1.035,
-        costMultiplier: 1.11,
-    },
-    {
-        name: "panaderia",
-        cost: document.querySelector(".panaderia-cost"),
-        parsedCost: parseFloat(document.querySelector(".panaderia-cost").innerHTML),
-        increase: document.querySelector(".panaderia-increase"),
-        parsedIncrease: parseFloat(document.querySelector(".panaderia-increase").innerHTML),
-        level: document.querySelector(".panaderia-level"),
-        tortaMultiplier: 1.04,
-        costMultiplier: 1.10,
-    },
-]
+const tortaclick = document.getElementById("tortaclick");
+tortaclick.addEventListener("click", aumentartortaf );
 
-function incrementFrita(event) {
+function aumentartortaf(event) {
     torta.innerHTML =  Math.round(parsedTorta += tpc);
 
     const x = event.offsetX
@@ -74,7 +36,7 @@ const timeout = (div) => {
     }, 800)
 }
 
-function buyUpgrade(upgrade) {
+function comprarMejora(upgrade) {
     const mu = upgrades.find((u) => {
         if (u.name === upgrade) return u
     })
@@ -95,7 +57,11 @@ function buyUpgrade(upgrade) {
             tps += mu.parsedIncrease
         }
     }
-} //mu = matchedUpgrades
+}; //mu = matchedUpgrades
+
+const btnG = document.querySelector("#guardar");
+
+btnG.addEventListener("click", save);
 
 function save () {
     localStorage.clear()
@@ -115,23 +81,27 @@ function save () {
     localStorage.setItem('tortas',JSON.stringify(parsedTorta))
 }
 
-function load () {
-    upgrades.map((upgrade) => {
-        const savedValues = JSON.parse(localStorage.getItem(upgrade.name))
+const btnC = document.querySelector("#cargar");
 
-        upgrade.parsedCost = savedValues.parsedCost
-        upgrade.parsedIncrease = savedValues.parsedIncrease
+btnC.addEventListener("click", load);
 
-        upgrade.level.innerHTML = savedValues.parsedLevel
-        upgrade.cost.innerHTML = Math.round(upgrade.parsedCost)
-        upgrade.increase.innerHTML = upgrade.parsedIncrease
-    })
-
-    tpc = JSON.parse(localStorage.getItem('tpc'))
-    tps = JSON.parse(localStorage.getItem('tps'))
-    parsedTorta = JSON.parse(localStorage.getItem('tortas'))
-
-    torta.innerHTML = Math.round(parsedTorta)
+    function load () {
+        upgrades.map((upgrade) => {
+            const savedValues = JSON.parse(localStorage.getItem(upgrade.name))
+    
+            upgrade.parsedCost = savedValues.parsedCost
+            upgrade.parsedIncrease = savedValues.parsedIncrease
+    
+            upgrade.level.innerHTML = savedValues.parsedLevel
+            upgrade.cost.innerHTML = Math.round(upgrade.parsedCost)
+            upgrade.increase.innerHTML = upgrade.parsedIncrease
+        })
+    
+        tpc = JSON.parse(localStorage.getItem('tpc'))
+        tps = JSON.parse(localStorage.getItem('tps'))
+        parsedTorta = JSON.parse(localStorage.getItem('tortas'))
+    
+        torta.innerHTML = Math.round(parsedTorta)
 }
 
 setInterval (() => {
@@ -140,3 +110,8 @@ setInterval (() => {
     tpcText.innerHTML = Math.round(tpc)
     tpsText.innerHTML = Math.round(tps)
 }, 100)
+
+window.aumentartortaf = aumentartortaf
+window.comprarMejora = comprarMejora
+window.save = save
+window.load = load
