fc4737bccdd7d29b693e75e1cdf32ef10d54927b
Martin Galeano <galeano.m2000@gmail.com>
2024-11-05 05:45:50 -0300

    Final

--- js/constantes/valoresmejoras.js
+++
@@ -1,7 +0,0 @@
-export const valoresmejoras = [
-    {name:'Clicker', image: '../../img/clicker-white.png', cost: 10, increase: 1},
-    {name:'Palo de Amasar', image: '../../img/pamasar.png', cost: 60, increase: 4},
-    {name:'Panadero', image: '../../img/panadero.png', cost: 380, increase: 16},
-    {name:'Panadería', image: '../../img/panaderia.png', cost: 3105, increase: 82},
-    {name:'Festival', image: '../../img/festival.png', cost: 23105, increase: 422},
-]
--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-import { upgrades } from "./constantes/upgrades.js"
-
 let torta = document.querySelector(".torta-cost")
 let parsedTorta = parseFloat(torta.innerHTML)
 
@@ -10,6 +8,59 @@ let tortaImgContainer = document.querySelector(".torta-img-container")
 let tpc = 1;
 
 let tps = 0;
+
+const upgrades = [
+    {
+        name: "clicker",
+        cost: document.querySelector(".clicker-cost"),
+        parsedCost: parseFloat(document.querySelector(".clicker-cost").innerHTML),
+        increase: document.querySelector(".clicker-increase"),
+        parsedIncrease: parseFloat(document.querySelector(".clicker-increase").innerHTML),
+        level: document.querySelector(".clicker-level"),
+        tortaMultiplier: 1.025,
+        costMultiplier: 1.12,
+    },
+    {
+        name: "pamasar",
+        cost: document.querySelector(".pamasar-cost"),
+        parsedCost: parseFloat(document.querySelector(".pamasar-cost").innerHTML),
+        increase: document.querySelector(".pamasar-increase"),
+        parsedIncrease: parseFloat(document.querySelector(".pamasar-increase").innerHTML),
+        level: document.querySelector(".pamasar-level"),
+        tortaMultiplier: 1.03,
+        costMultiplier: 1.115,
+    },
+    {
+        name: "panadero",
+        cost: document.querySelector(".panadero-cost"),
+        parsedCost: parseFloat(document.querySelector(".panadero-cost").innerHTML),
+        increase: document.querySelector(".panadero-increase"),
+        parsedIncrease: parseFloat(document.querySelector(".panadero-increase").innerHTML),
+        level: document.querySelector(".panadero-level"),
+        tortaMultiplier: 1.035,
+        costMultiplier: 1.11,
+    },
+    {
+        name: "panaderia",
+        cost: document.querySelector(".panaderia-cost"),
+        parsedCost: parseFloat(document.querySelector(".panaderia-cost").innerHTML),
+        increase: document.querySelector(".panaderia-increase"),
+        parsedIncrease: parseFloat(document.querySelector(".panaderia-increase").innerHTML),
+        level: document.querySelector(".panaderia-level"),
+        tortaMultiplier: 1.04,
+        costMultiplier: 1.10,
+    },
+    {
+        name: "festival",
+        cost: document.querySelector(".festival-cost"),
+        parsedCost: parseFloat(document.querySelector(".festival-cost").innerHTML),
+        increase: document.querySelector(".festival-increase"),
+        parsedIncrease: parseFloat(document.querySelector(".festival-increase").innerHTML),
+        level: document.querySelector(".festival-level"),
+        tortaMultiplier: 1.045,
+        costMultiplier: 1.09,
+    },
+]
 
 const tortaclick = document.getElementById("tortaclick");
 tortaclick.addEventListener("click", aumentartortaf );
@@ -110,8 +161,3 @@ setInterval (() => {
     tpcText.innerHTML = Math.round(tpc)
     tpsText.innerHTML = Math.round(tps)
 }, 100)
-
-window.aumentartortaf = aumentartortaf
-window.comprarMejora = comprarMejora
-window.save = save
-window.load = load
